029e0750a7312a1a855256a4b874a2663eb43c4e
gabeeck <eck.gabe@gmail.com>
2019-01-26 13:37:12 -0800

    Creep Prefab/Spawning
    Added a folder for all of the creep prefabs. Added a spawning function that takes a Vector2D.

--- Assets/Scripts/CreepAI.cs
+++ Assets/Scripts/CreepAI.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class CreepAI : MonoBehaviour
 {
     public float progress { get; private set; } = 0;
+    [SerializeField] public float health = 100;
     private float speed = 5;
+    private bool death = false;
+    private bool spawned = false;
+    [SerializeField] public Vector2 spawnPoint = new Vector2(0,0);
     
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
         progress += Time.deltaTime * speed;
+    }
+
+    void Spawn()
+    {
+        this.GetComponent<Rigidbody2D>().MovePosition(spawnPoint);
+        spawned = true;
     }
 }

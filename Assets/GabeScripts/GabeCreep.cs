e9e22a846ca423d0710fe70e709dbe397d6b4e18
gabeeck <eck.gabe@gmail.com>
2019-01-26 13:41:08 -0800

    Gabe Creep script
    Added another creep script to avoid conflicts with CreepAI

--- Assets/GabeScripts/GabeCreep.cs
+++ Assets/GabeScripts/GabeCreep.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreepAI : MonoBehaviour
+public class GabeCreep : MonoBehaviour
 {
     public float progress { get; private set; } = 0;
+    [SerializeField] public float health = 100;
     private float speed = 5;
-    
+    private bool death = false;
+    private bool spawned = false;
+    [SerializeField] public Vector2 spawnPoint = new Vector2(0, 0);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
         progress += Time.deltaTime * speed;
+    }
+
+    void Spawn()
+    {
+        this.GetComponent<Rigidbody2D>().MovePosition(spawnPoint);
+        spawned = true;
     }
 }

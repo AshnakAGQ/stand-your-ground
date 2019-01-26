1016285c657276e829d5164a648f9c9ccdc32959
gabeeck <eck.gabe@gmail.com>
2019-01-26 14:04:24 -0800

    Death and Health for Creep
    Added a death function that deletes the creep when its health gets to zero or less

--- Assets/GabeScripts/GabeCreep.cs
+++ Assets/GabeScripts/GabeCreep.cs
@@ -7,7 +7,7 @@ public class GabeCreep : MonoBehaviour
     public float progress { get; private set; } = 0;
     [SerializeField] public float health = 100;
     private float speed = 5;
-    private bool death = false;
+    [SerializeField] public bool dead = false;
     private bool spawned = false;
     [SerializeField] public Vector2 spawnPoint = new Vector2(0, 0);
 
@@ -15,18 +15,28 @@ public class GabeCreep : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Spawn();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         progress += Time.deltaTime * speed;
+        checkDeath();
     }
 
     void Spawn()
     {
         this.GetComponent<Rigidbody2D>().MovePosition(spawnPoint);
         spawned = true;
+    }
+
+    void checkDeath()
+    {
+        if (health <= 0)
+        {
+            dead = true;
+            Destroy(gameObject);
+        }
     }
 }

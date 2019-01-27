b7b41758d8998e9b3774ebc82263ac8c065211a4
gabeeck <eck.gabe@gmail.com>
2019-01-26 17:11:03 -0800

    Yard tiles and creep movement
    Added a collection of yard tiles. Added movement to creeps and gave them pathfinding for tiles tagged as path.

---
+++ Assets/GabeScripts/Path.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] public bool visited = false;
+    [SerializeField] public bool endTile = false;
+    [SerializeField] public Vector2 tilePosition;
+    [SerializeField] public float tilePositionX;
+    [SerializeField] public float tilePositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilePosition = transform.position;
+        tilePositionX = transform.position.x;
+        tilePositionY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
